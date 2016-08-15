a5d392066b4c479cbc75a7d795e7426a2a93de06
jpila <j.pilapil@hotmail.com>
2016-08-15 13:24:56 -0400

    modified data model to include inspections

---
+++ Inspection+CoreDataProperties.m
@@ -0,0 +1,21 @@
+//
+//  Inspection+CoreDataProperties.m
+//  
+//
+//  Created by JOSE PILAPIL on 2016-08-15.
+//
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "Inspection+CoreDataProperties.h"
+
+@implementation Inspection (CoreDataProperties)
+
+@dynamic date;
+@dynamic name;
+@dynamic rooms;
+@dynamic house;
+
+@end
